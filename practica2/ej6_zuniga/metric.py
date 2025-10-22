1a0c6e9d6330608eca897ed25fdbade8125719d5
Laura Zúñiga <108493878+lzunigao@users.noreply.github.com>
2025-10-22 16:19:43 -0300

    metric

--- practica2/ej6_zuniga/metric.py
+++ practica2/ej6_zuniga/metric.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 def accuracy(y_true, y_pred): #accuracy del XOR. Las salidas tienen un solo bit
-    return np.mean(y_true == y_pred, axis=0)
+    return np.mean(y_true == y_pred, axis=-1)
 
 def mse(y_true, y_pred): #mean squared error
     assert y_true.shape == y_pred.shape, "Shapes of y_true and y_pred must be the same."
-    return np.sqrt (np.sum((y_true - y_pred) ** 2))
-
+    return np.mean((y_true - y_pred) ** 2, axis=-1)
